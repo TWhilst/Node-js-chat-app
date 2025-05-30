af05192af5ea41fb81e999549fd0d1628147aba1
TWhilst <tochethestrict@gmail.com>
2025-05-30 02:05:34 +0100

    made changes

--- app.js
+++ app.js
@@ -2,10 +2,15 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', socket => {
     console.log('User connected');
